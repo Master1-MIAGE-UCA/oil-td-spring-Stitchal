0b719a33b6a4d522c404123f5fd7b645bc255c32
Alexis Rosset <alexis.rosset@etu.unice.fr>
2024-12-13 08:50:19 +0100

    Création du contrôleur REST pour lancer les dés

---
+++ dice/src/main/java/fr/unica/miage/rosset/dice/Controller.java
@@ -0,0 +1,26 @@
+package fr.unica.miage.rosset.dice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/rollDice")
+    public String rollDice() {
+        Dice dice = new Dice();
+        dice.roll();
+        return "The dice rolled a " + dice.getValue();
+    }
+
+    @GetMapping("/rollDice/{X}")
+    public String rollDiceX(int X) {
+        Dice dice = new Dice();
+        int sum = 0;
+        for (int i = 0; i < X; i++) {
+            dice.roll();
+            sum += dice.getValue();
+        }
+        return "The dice rolled a sum of " + sum;
+    }
+}
